1a5da5261080912993f6c455cdb63629a7eba3f5
Vytautas Astrauskas <vastrauskas@gmail.com>
2016-11-07 18:06:40 +0100

    Added layer printing.

--- exportlayers.py
+++ exportlayers.py
@@ -8,7 +8,7 @@ from xml.dom import minidom
 import codecs
 
 
-def export_layers(src, dest, hide, show):
+def export_layers(src, dest, hide, show, hide_all, print_layers):
     """
     Export selected layers of SVG in the file `src` to the file `dest`.
 
@@ -24,16 +24,19 @@ def export_layers(src, dest, hide, show):
     for g in svg.getElementsByTagName("g"):
         if g.attributes.has_key("inkscape:label"):
             label = g.attributes["inkscape:label"].value
-            if label in hide:
+            if print_layers:
+                print(label)
+            if label in hide or hide_all:
                 g.attributes['style'] = 'display:none'
                 g_hide.append(g)
             elif label in show:
                 g.attributes['style'] = 'display:inline'
                 g_show.append(g)
     export = svg.toxml()
-    codecs.open(dest, "w", encoding="utf8").write(export)
-    print "Hide {0} node(s);  Show {1} node(s).".format(
-        len(g_hide), len(g_show))
+    if dest:
+        codecs.open(dest, "w", encoding="utf8").write(export)
+        print("Hide {0} node(s);  Show {1} node(s).".format(
+            len(g_hide), len(g_show)))
 
 
 def main():
@@ -43,10 +46,17 @@ def main():
         '--hide', action='append', default=[],
         help='layer to hide. this option can be specified multiple times.')
     parser.add_argument(
+        '--hide_all', action='store_true',
+        help='hide all layers.')
+    parser.add_argument(
+        '--print_layers', action='store_true',
+        help='print a list of layers in a file.')
+    parser.add_argument(
         '--show', action='append', default=[],
         help='layer to show. this option can be specified multiple times.')
     parser.add_argument('src', help='source SVG file.')
-    parser.add_argument('dest', help='path to export SVG file.')
+    parser.add_argument('dest', nargs='?', default=None,
+        help='path to export SVG file.')
     args = parser.parse_args()
     export_layers(**vars(args))
 
